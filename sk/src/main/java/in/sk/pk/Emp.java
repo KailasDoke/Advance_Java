4fe0553b22fbb7406907bdc057952d88f7bb28b5
Kailas <kailasdoke100@gmail.com>
2026-04-26 23:17:57 +0530

    Servlet Example

---
+++ sk/src/main/java/in/sk/pk/Emp.java
@@ -0,0 +1,25 @@
+package in.sk.pk;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet("/aaa")
+public class Emp  extends HttpServlet{
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter ps = resp.getWriter();
+		ps.println("hi this is doke");
+	}
+
+}
