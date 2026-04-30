54c22585d1992ce7cdd7eee199d1e6f5c2120cb7
Kailas <kailasdoke100@gmail.com>
2026-04-30 18:28:00 +0530

    Registration and Login  + Servlet + JSP + JDBC + MySql

---
+++ RegisterLogin/src/main/java/in/kd/backend/Login.java
@@ -0,0 +1,66 @@
+package in.kd.backend;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+@WebServlet("/login")
+public class Login extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.sendRedirect("Login.jsp"); 
+	}
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		 String myemail = req.getParameter("email");
+		 String mypass = req.getParameter("password");
+		 
+		 try {
+			Class.forName("com.mysql.cj.jdbc.Driver");
+			Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/reg","root","Kailas@2004");
+		
+		    PreparedStatement ps = con.prepareStatement("select * from register where email=? and pass=?");
+		    ps.setString(1, myemail);
+		    ps.setString(2, mypass);
+		    
+		    ResultSet rs = ps.executeQuery();
+		    
+		    if(rs.next())
+		    {
+		    	HttpSession session = req.getSession();
+		    	session.setAttribute("session_name", rs.getString("name"));
+		    	RequestDispatcher rd = req.getRequestDispatcher("/Profile.jsp");
+		    	rd.include(req,resp);
+		    }
+		    else
+		    {
+		    	resp.setContentType("text/html");
+		    	out.print("<h3 style='color:red'> Email id and password doesent match</h3>");
+		    	RequestDispatcher rd = req.getRequestDispatcher("/Login.jsp");
+		    	rd.include(req, resp);
+		    }
+		 } 
+		 
+		catch (Exception e) {
+			e.printStackTrace();
+			resp.setContentType("text/html");
+	    	out.print("<h3 style='color:red'> "+ e.getMessage()+"</h3>");
+	    	RequestDispatcher rd = req.getRequestDispatcher("/Login.jsp");
+	    	rd.include(req, resp);
+				
+		}
+	}
+
+}
---
+++ RegisterLogin/src/main/webapp/Login.jsp
@@ -0,0 +1,17 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Insert title here</title>
+</head>
+<body>
+	<form action="login" method="post">
+	Email: <input type="text" name="email" placeholder="Enter your email"><br/><br/>
+	Password: <input type="password" name="password" placeholder="Enter your password "  ><br/><br/>
+	<input type="submit" value="login">
+	</form>
+
+</body>
+</html>
